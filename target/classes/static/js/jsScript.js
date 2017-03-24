b9468dba825f29ded06fdf4ce43d08fbea16e849
PatrikRiku <patrik@riku.se>
2017-03-24 10:19:05 +0100

    hejhej

--- target/classes/static/js/jsScript.js
+++ target/classes/static/js/jsScript.js
@@ -1,4 +1,5 @@
 // jQuery för att diven alltid ska ligga centralt horisontellt
+/*
 $( document ).ready(function() {
   var height = $(window).height();
   var divHeight = $("#main-content").outerHeight();
@@ -12,4 +13,26 @@ $( window ).resize(function() {
   height = $(window).height();
 	divHeight = $("#main-content").outerHeight();
 	$("#main-content").css("margin-top",(height-divHeight)/2);
+});
+*/
+$( document ).ready(function() {
+  var height = $(window).height();
+  var divHeight = $("#main-content").outerHeight();
+  var SetMarginTop = (height-divHeight)/2;
+  if(SetMarginTop < 50) { SetMarginTop = 50; }
+  $("#main-content").css("margin-top",SetMarginTop);
+});
+$( window ).resize(function() {
+  height = $(window).height();
+  divHeight = $("#main-content").outerHeight();
+  SetMarginTop = (height-divHeight)/2;
+  if(SetMarginTop < 50) { SetMarginTop = 50; }
+  $("#main-content").css("margin-top",SetMarginTop);
+});
+$("#main-content").resize(function() {
+  height = $(window).height();
+  divHeight = $("#main-content").outerHeight();
+  SetMarginTop = (height-divHeight)/2;
+  if(SetMarginTop < 50) { SetMarginTop = 50; }
+  $("#main-content").css("margin-top",SetMarginTop);
 });
